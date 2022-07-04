b305b62351676f41fab57190d47029468dc9ef20
dharmilorla <babarinde.damilola@gmail.com>
2022-07-04 15:47:58 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,13 +1,30 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 /**
- * main - Prints all single digit numbers of base 10 starting from 0
+ * main - Entry point
+ * Description: Alphabets
  * Return: Always 0
  */
 int main(void)
 {
-int num;
-for (num = 0; num < 10; num++)
-printf("%d", num);
-printf("\n");
+char letter = 'a';
+while (letter <= 'z') 
+{
+if (letter == 'e')
+{
+letter++;
+}
+else if (letter == 'q')
+{
+letter++;
+}
+else
+{
+putchar(letter);
+letter++;
+}
+}
+putchar('\n');
 return (0);
 }
