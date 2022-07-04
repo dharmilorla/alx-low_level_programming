d265be6c3476819b65247766eea258bae864cac4
dharmilorla <babarinde.damilola@gmail.com>
2022-07-04 16:44:34 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+char letter = 'z';
+while (letter >= 'a')
+{
+putchar(letter);
+letter--;
+}
+pitchar('\n');
+return (0);
+}
