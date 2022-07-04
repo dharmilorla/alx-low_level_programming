4460bc508149aaea7c8dd75f3ab6bb5d965f08d2
dharmilorla <babarinde.damilola@gmail.com>
2022-07-04 16:00:50 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Alphabets
+ * Return: Always 0
+ */
+int main(void)
+{
+char num = '0';
+while (num <= '9')
+{
+putchar(num);
+num++;
+}
+putchar('\n');
+return (0);
