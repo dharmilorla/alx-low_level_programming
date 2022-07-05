20d8d31b0e2f7023bdbecc961cb092d3bbc011df
dharmilorla <babarinde.damilola@gmail.com>
2022-07-04 18:04:38 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - writes 00 - 99
+ * Description: write unique combination
+ * Return: 0 on success
+ */
+int main(void)
+{
+int i, j;
+for (i = '0'; i <= '9'; i++)
+{
+for (j = '0'; j <= '9'; j++)
+{
+if ((i < j) & (j <= '0'))
+{
+putchar(i);
+putchar(j);
+if ((j < '9') | (i < '8'))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchart('\n');
+return (0);
+}
