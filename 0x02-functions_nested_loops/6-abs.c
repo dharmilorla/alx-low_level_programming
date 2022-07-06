8035ca256e954b372b72056de4824586b3e2eee0
dharmilorla <babarinde.damilola@gmail.com>
2022-07-06 16:53:59 -0700

    Task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 
 /**
  * _abs - function that computes the absolute value of an integer
- * @c:  is the int that will use for the argument of the function
+ * @n:  is the int that will use for the argument of the function
  * Return: 0 or 1
  */
 
