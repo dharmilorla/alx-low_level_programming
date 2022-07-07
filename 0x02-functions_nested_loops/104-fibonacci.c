19bb673f2103e5842542b77c49071df57dd69d3c
dharmilorla <babarinde.damilola@gmail.com>
2022-07-07 16:36:37 -0700

    Task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2
+ * Return: ALways 0 (Success)
+ */
+
+int main(void)
+{
+unsigned long int i, j, k, j1, j2, k1, k2;
+
+j = 1;
+k = 2;
+
+printf("%lu", j);
+
+for (i = 1; i < 91; i++)
+{
+	printf(", %lu", k);
+	k = k + j;
+	j = k - j;
+}
+
+return (0);
+}
