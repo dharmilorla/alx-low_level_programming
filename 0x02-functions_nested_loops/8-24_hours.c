d780473f05bfd71f69e9d9cb0c6c6cb7942e2bd2
dharmilorla <babarinde.damilola@gmail.com>
2022-07-07 14:46:45 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,43 +8,26 @@
 void jack_bauer(void)
 {
 
-int h1;
-int h2;
-int m1;
-int m2;
-int a = 9;
+int hours = 0;
+int minutes = 0;
+int hours_remainder;
+int mins_remainder;
 
-h2 = 0;
-while (h2 <= 2)
+while (hours <= 23)
 {
-	if (h2 == 2)
+	while (minutes <= 59)
 	{
-		/* Restrain to 23hr, not 29*/
-		a = 3;
+	mins_remainder = minutes % 10;
+	hours_remainder = hours % 10;
+	_putchar(hours / 10 + '0');
+	_putchar(hours_remainder + '0');
+	_putchar(':');
+	_putchar(minutes / 10 + '0');
+	_putchar(mins_remainder + '0');
+	minutes++;
+	_putchar('\n');
 	}
-	h1 = 0;
-	while (h1 <= a);
-	{
-		m2 = 0;
-		while (m2 <= 5)
-		{
-			m1 = 0;
-			while (m1 <= 9)
-			{
-				_putchar('0' + h2);
-				_putchar('0' + h1);
-				_putchar(':');
-				_putchar('0' + m2);
-				_putchar('0' + m1);
-				_putchar('\n');
-				m1++;
-			}
-			m2++;
-		}
-		h1++;
-	}
-	h2++;
+	hours++;
+	minutes = 0;
 }
-
 }
-
