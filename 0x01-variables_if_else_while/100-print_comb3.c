51d7e64e83a0161552e9a260477d847bc787bb00
dharmilorla <babarinde.damilola@gmail.com>
2022-07-08 07:23:35 -0700

    Task 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,51 +7,26 @@
  */
 
 int main(void)
-
 {
-
 	int ch;
-
 	int n;
-
 	for (ch = 48; ch <= 57; ch++)
-
 	{
-
 		for (n = 49; n <= 57; n++)
-
 		{
-
 			if (n > ch)
-
 			{
-
 				putchar(ch);
-
 				putchar(n);
-
 				if (ch != 56 || n != 57)
-
 				{
-
 					putchar(44);
-
 					putchar(32);
-
 				}
-
 			}
-
 		}
-
 	}
-
-
-
 	putchar(10); /* this is an ascii code for new line*/
 
-
-
 	return (0);
-
 }
